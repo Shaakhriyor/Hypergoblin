9eb0f711814582bb795322359179bc831186832e
Jauhelihamies <s2401274@edu.bc.fi>
2026-08-28 10:21:18 +0300

    Math Fix on score board

--- Assets/Match_3_Export/Export/Scripts/Score.cs
+++ Assets/Match_3_Export/Export/Scripts/Score.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreO : MonoBehaviour
 {
     public int score = 0;
     public TMP_Text scoreText;
-
+    public int Taso = 1;
+    public int Check = 0;
     void Start()
     {
         UpdateScoreUI();
@@ -13,8 +15,25 @@ public class ScoreO : MonoBehaviour
 
     public void AddScore()
     {
-        score++;
-        UpdateScoreUI();
+        FixScore();
+       
+
+    }
+    public void FixScore()
+    {
+        Check++;
+        if (Check == 3)
+        {
+            score += 10;
+            UpdateScoreUI();
+            if (score >= 100)
+            {
+                SceneManager.LoadScene(Taso);
+            }
+            Check = 0;
+        }
+        
+
     }
 
     void UpdateScoreUI()
