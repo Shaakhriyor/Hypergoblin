9facb3e645720201c6e97a31ff3e3fc99f549eae
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-09-02 10:33:45 +0300

    .

--- Assets/Scripts/CrowdManager.cs
+++ Assets/Scripts/CrowdManager.cs
@@ -1,38 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class CrowdManager : MonoBehaviour
 {
     public static CrowdManager Instance;
 
-    [Header("UI")]
-    public TMP_Text crowdText;
-
     [Header("Goblin Setup")]
-    public GameObject[] goblinPrefabs;
-    public GameObject giantGoblinPrefab;
-    public int mergeThreshold = 10;
-    public float giantScaleMultiplier = 2.0f;
-
-    [Header("Path Following")]
-    public float followSpeed = 15f;
-    public float recordDistance = 0.1f;
+    public GameObject[] goblinPrefabs; // Assign multiple Goblin Prefabs here!
+    public List<GameObject> activeGoblins = new List<GameObject>();
 
     [Header("Cluster & Boundaries")]
-    public float clusterWidth = 1.5f;
-    public float maxForwardOffset = 0.5f;
-    public int maxGoblinsInFront = 3;
+    public float clusterRadius = 0.8f;
     public float limitX = 4f;
-
-    public List<GameObject> activeGoblins = new List<GameObject>();
-    public List<GameObject> activeGiantGoblins = new List<GameObject>();
-
-    private List<Vector3> smallOffsets = new List<Vector3>();
-    private List<Vector3> giantOffsets = new List<Vector3>();
-
-    private List<Vector3> pathHistory = new List<Vector3>();
-    private Vector3 lastRecordedPos;
 
     void Awake()
     {
@@ -44,139 +23,76 @@ public class CrowdManager : MonoBehaviour
         if (!activeGoblins.Contains(gameObject))
         {
             activeGoblins.Add(gameObject);
-            smallOffsets.Add(Vector3.zero);
-        }
-
-        lastRecordedPos = transform.position;
-        pathHistory.Add(transform.position);
-    }
-
-    void Update()
-    {
-        if (crowdText != null)
-        {
-            int totalValue = activeGoblins.Count + (activeGiantGoblins.Count * mergeThreshold);
-            crowdText.text = totalValue.ToString();
         }
     }
 
     void LateUpdate()
     {
-        if (Vector3.Distance(transform.position, lastRecordedPos) >= recordDistance)
-        {
-            pathHistory.Insert(0, transform.position);
-            lastRecordedPos = transform.position;
-
-            if (pathHistory.Count > 500)
-            {
-                pathHistory.RemoveAt(pathHistory.Count - 1);
-            }
-        }
-
-        for (int i = 1; i < activeGoblins.Count; i++)
+        for (int i = 0; i < activeGoblins.Count; i++)
         {
             if (activeGoblins[i] == null) continue;
-            MoveGoblinAlongPath(activeGoblins[i], smallOffsets[i]);
+
+            Vector3 pos = activeGoblins[i].transform.position;
+            pos.x = Mathf.Clamp(pos.x, -limitX, limitX);
+            activeGoblins[i].transform.position = pos;
         }
-
-        for (int i = 0; i < activeGiantGoblins.Count; i++)
-        {
-            if (activeGiantGoblins[i] == null) continue;
-            MoveGoblinAlongPath(activeGiantGoblins[i], giantOffsets[i]);
-        }
-    }
-
-    void MoveGoblinAlongPath(GameObject goblin, Vector3 offset)
-    {
-        Vector3 basePos;
-
-        if (offset.z >= 0 || pathHistory.Count == 0)
-        {
-            basePos = transform.position + new Vector3(0, 0, offset.z);
-        }
-        else
-        {
-            int historyStepsBack = Mathf.RoundToInt(Mathf.Abs(offset.z) / recordDistance);
-            int index = Mathf.Clamp(historyStepsBack, 0, pathHistory.Count - 1);
-            basePos = pathHistory[index];
-        }
-
-        Vector3 targetWorldPos = basePos + new Vector3(offset.x, 0, 0);
-
-        Vector3 newPos = Vector3.Lerp(goblin.transform.position, targetWorldPos, Time.deltaTime * followSpeed);
-        newPos.x = Mathf.Clamp(newPos.x, -limitX, limitX);
-
-        goblin.transform.position = newPos;
     }
 
     public void ApplyGateMath(GateType type, int value)
     {
-        int currentSmallCount = activeGoblins.Count - 1;
+        int currentCount = activeGoblins.Count;
+        int newTotal = currentCount;
 
         switch (type)
         {
             case GateType.Add:
-                AddSmallGoblins(value);
+                newTotal += value;
                 break;
             case GateType.Multiply:
-                int amountToAdd = (currentSmallCount * value) - currentSmallCount;
-                AddSmallGoblins(amountToAdd);
+                newTotal *= value;
                 break;
             case GateType.Subtract:
-                RemoveSmallGoblins(value);
+                newTotal -= value;
                 break;
             case GateType.Divide:
-                if (value > 0)
-                {
-                    int newCount = currentSmallCount / value;
-                    RemoveSmallGoblins(currentSmallCount - newCount);
-                }
+                if (value > 0) newTotal /= value;
                 break;
         }
 
-        CheckForMerge();
+        newTotal = Mathf.Max(1, newTotal);
+        int difference = newTotal - currentCount;
+
+        if (difference > 0)
+        {
+            SpawnGoblins(difference);
+        }
+        else if (difference < 0)
+        {
+            RemoveGoblins(Mathf.Abs(difference));
+        }
     }
 
-    void AddSmallGoblins(int count)
+    void SpawnGoblins(int count)
     {
-        int currentInFront = 0;
-        for (int j = 1; j < smallOffsets.Count; j++)
-        {
-            if (smallOffsets[j].z > 0.1f)
-            {
-                currentInFront++;
-            }
-        }
+        if (goblinPrefabs == null || goblinPrefabs.Length == 0) return;
 
         for (int i = 0; i < count; i++)
         {
-            float xOffset = Random.Range(-clusterWidth, clusterWidth);
-            float zOffset;
+            Vector2 randomCircle = Random.insideUnitCircle * (clusterRadius + (activeGoblins.Count * 0.03f));
+            Vector3 spawnOffset = new Vector3(randomCircle.x, 0, randomCircle.y);
 
-            if (currentInFront < maxGoblinsInFront)
-            {
-                zOffset = Random.Range(0.1f, maxForwardOffset);
-                currentInFront++;
-            }
-            else
-            {
-                float maxBackwardOffset = 0.5f + ((activeGoblins.Count + activeGiantGoblins.Count) * 0.08f);
-                zOffset = Random.Range(-maxBackwardOffset, -0.1f);
-            }
+            // Pick a random goblin variant from the array
+            int randomIndex = Random.Range(0, goblinPrefabs.Length);
+            GameObject chosenPrefab = goblinPrefabs[randomIndex];
 
-            Vector3 spawnOffset = new Vector3(xOffset, 0, zOffset);
-            GameObject prefabToUse = GetRandomPrefab();
+            GameObject newGoblin = Instantiate(chosenPrefab, transform.position + spawnOffset, Quaternion.identity);
+            newGoblin.transform.SetParent(transform);
 
-            if (prefabToUse != null)
-            {
-                GameObject newGoblin = Instantiate(prefabToUse, transform.position + spawnOffset, Quaternion.identity);
-                activeGoblins.Add(newGoblin);
-                smallOffsets.Add(spawnOffset);
-            }
+            activeGoblins.Add(newGoblin);
         }
     }
 
-    void RemoveSmallGoblins(int count)
+    void RemoveGoblins(int count)
     {
         for (int i = 0; i < count; i++)
         {
@@ -186,68 +102,8 @@ public class CrowdManager : MonoBehaviour
                 GameObject goblinToRemove = activeGoblins[lastIndex];
 
                 activeGoblins.RemoveAt(lastIndex);
-                smallOffsets.RemoveAt(lastIndex);
                 Destroy(goblinToRemove);
             }
-            else if (activeGiantGoblins.Count > 0)
-            {
-                int lastGiantIndex = activeGiantGoblins.Count - 1;
-                GameObject giantToRemove = activeGiantGoblins[lastGiantIndex];
-
-                activeGiantGoblins.RemoveAt(lastGiantIndex);
-                giantOffsets.RemoveAt(lastGiantIndex);
-                Destroy(giantToRemove);
-
-                AddSmallGoblins(mergeThreshold - 1);
-            }
         }
-    }
-
-    void CheckForMerge()
-    {
-        while (activeGoblins.Count - 1 >= mergeThreshold)
-        {
-            for (int i = 0; i < mergeThreshold; i++)
-            {
-                int lastIndex = activeGoblins.Count - 1;
-                GameObject goblinToRemove = activeGoblins[lastIndex];
-
-                activeGoblins.RemoveAt(lastIndex);
-                smallOffsets.RemoveAt(lastIndex);
-                Destroy(goblinToRemove);
-            }
-
-            SpawnGiantGoblin();
-        }
-    }
-
-    void SpawnGiantGoblin()
-    {
-        float xOffset = Random.Range(-clusterWidth, clusterWidth);
-        float maxBackwardOffset = 0.5f + ((activeGoblins.Count + activeGiantGoblins.Count) * 0.08f);
-        float zOffset = Random.Range(-maxBackwardOffset, -0.1f);
-
-        Vector3 spawnOffset = new Vector3(xOffset, 0, zOffset);
-        GameObject prefabToUse = giantGoblinPrefab != null ? giantGoblinPrefab : GetRandomPrefab();
-
-        if (prefabToUse != null)
-        {
-            GameObject giant = Instantiate(prefabToUse, transform.position, Quaternion.identity);
-
-            if (giantGoblinPrefab == null)
-            {
-                giant.transform.localScale *= giantScaleMultiplier;
-            }
-
-            activeGiantGoblins.Add(giant);
-            giantOffsets.Add(spawnOffset);
-        }
-    }
-
-    GameObject GetRandomPrefab()
-    {
-        if (goblinPrefabs == null || goblinPrefabs.Length == 0) return null;
-        int randomIndex = Random.Range(0, goblinPrefabs.Length);
-        return goblinPrefabs[randomIndex];
     }
 }
